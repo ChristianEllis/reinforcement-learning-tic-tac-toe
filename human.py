343631d38d859f1de70bb33f9c8451de0308acca
ChristianEllis <christian.ellis96@gmail.com>
2020-04-27 16:01:39 -0400

    added requirements

--- human.py
+++ human.py
@@ -1,14 +1,17 @@
+# Custom Imports
+from game import TicTacToe
+
 class Human(object):
   def __init__(self, player):
     self.player = player
 
   def action(self, state):
-    printboard(state)
+    TicTacToe.print_board(state)
     action = input('Your move? i.e. x,y : ')
     return (int(action.split(',')[0]),int(action.split(',')[1]))
 
   def episode_over(self, winner):
-    if winner == DRAW:
+    if winner == 0:
       print('Game over! It was a draw.')
     else:
       print('Game over! Winner: Player {0}'.format(winner))
